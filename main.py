929cef3d62139b3fa82b79abf0b1b75e4e720d26
mmopanemoroka@gmail.com <mmopanemoroka@gmail.com>
2024-09-21 18:03:15 +0200

    createdeck function

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('get started')
+# Create a deck of cards
+def createDeck():
+    deck = []
+    # Define the structure of the cards
+    cards = {
+        'Red': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'Skip', 'Draw Two', 'Reverse'],
+        'Blue': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'Skip', 'Draw Two', 'Reverse'],
+        'Green': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'Skip', 'Draw Two', 'Reverse'],
+        'Yellow': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'Skip', 'Draw Two', 'Reverse']
+    }
+    
+    # Add Wild cards
+    wilds = ['Wild', 'Wild Draw Four'] * 4
+    deck.extend(wilds)
+
+    # Add colored cards
+    for color, values in cards.items():
+        for value in values:
+            card = f"{color} {value}"
+            # Add card (for 0, only one copy, for others, two copies)
+            deck.append(card)
+            if value != 0:
+                deck.append(card)
+
+    print(len(deck))
+
+    # return deck
+
+createDeck()
