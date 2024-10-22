f11e84eaa5140f9600a96d5c0cd67ed14d74a39a
Kamogelo Mmopane <101883751+KAMO333@users.noreply.github.com>
2024-10-22 21:56:55 +0200

    current colour variable

--- main.py
+++ main.py
@@ -166,6 +166,7 @@ while playing:
 
                 while new_colour < 1 or new_colour > 4:
                     new_colour = int(float(input("Invalid choice. Please select a color to play. ")))
+                    current_colour = colours[new_colour-1]
 
 
 
